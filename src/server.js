8808be64ffaf346916b97d7d957a6017ced4c606
JDBERBEO <47923495+JDBERBEO@users.noreply.github.com>
2021-08-27 17:21:31 -0500

    Sets server (#1)
    * adding server dependecies, cors, morgan, nodemon, dotenv
    
    * creates database

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+
+
+const port = process.env.PORT || 8000
+const app = express()
+
+app.use(express.json)
+app.use(cors())
+app.use(morgan('dev'))
+
+
+app.listen(8000, () => {
+    console.log(`app running at http://localhost:${port}`)
+})
