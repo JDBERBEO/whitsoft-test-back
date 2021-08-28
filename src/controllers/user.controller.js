564c4f6d60730d31a5a3c51790a42c99d1e3043f
Juan David <jd.berbeo@gmail.com>
2021-08-28 12:46:58 -0500

    sends data of name input, shows error when is not filled by user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,10 +5,12 @@ module.exports = {
       console.log('controller activated')
       try {
         const { body } = req
+        console.log('body', body)
         const user = await User.create(body)
   
         res.status(201).json('user created')
       } catch (err) {
+        console.log(err.message)
         res.status(400).json({ message: err.message })
       }
     }
--- src/models/user.js
+++ src/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new Schema(
       name: {
         type: String,
         trim: true,
-        required: [true, 'name is required'],
+        required: [true,'name is required'],
       },
       country: {
         type: String,
