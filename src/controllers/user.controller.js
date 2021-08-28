b3434ca291cd973c3a9da7ae3dcf344d52def554
Juan David <jd.berbeo@gmail.com>
2021-08-28 15:51:13 -0500

    test-countries

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,10 @@ const User = require('../models/user')
 
 module.exports = {
     async signup(req, res) {
-      console.log('controller activated')
+
       try {
         const { body } = req
+
         const user = await User.create(body)
   
         res.status(201).json('user created')
