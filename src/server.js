17aae30aa32c1d84920b4b701c409157bbe6f7b6
JDBERBEO <47923495+JDBERBEO@users.noreply.github.com>
2021-08-27 22:04:21 -0500

    Creates model view controller pattern (#2)
    * adding server dependecies, cors, morgan, nodemon, dotenv
    
    * creates database
    
    * creates model and controller
    
    * corrects express Json, test with postman works

--- src/server.js
+++ src/server.js
@@ -2,15 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const connect = require('./db')
+const userRouter = require('./routes/user.routes')
 
 
 
 const port = process.env.PORT || 8000
 const app = express()
+connect()
 
-app.use(express.json)
+app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
+
+app.use('/user', userRouter)
 
 
 app.listen(8000, () => {
